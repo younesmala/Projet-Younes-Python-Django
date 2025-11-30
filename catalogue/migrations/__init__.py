b0e840a8efc535d72430ae0eb0680fcfe804f5d6
Younes <younesmala124@gmail.com>
2025-11-30 18:41:58 +0100

    fix: corrections complètes liées au StarterKit et au mapping relationnel

---
+++ catalogue/migrations/__init__.py
@@ -0,0 +1,23 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Artists',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('firstname', models.CharField(max_length=60)),
+                ('lastname', models.CharField(max_length=60)),
+            ],
+            options={
+                'db_table': 'artists',
+            },
+        ),
+    ]
